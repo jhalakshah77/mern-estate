81636cc5bedd05a2388a4bfc8cfa492a2a3d09f6
jhalakshah77 <jhalakshah77@gmail.com>
2024-04-05 16:44:53 +0400

    first commit with client and all prerequisites

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
