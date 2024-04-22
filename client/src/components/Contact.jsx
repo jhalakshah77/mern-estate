0e15f350896a36d4b966ad37f66d2252d50ac879
jhalakshah77 <jhalakshah77@gmail.com>
2024-04-22 17:34:51 +0400

    add contact landlord functionality to listing page

---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Contact({ listing }) {
+  const [landlord, setLandlord] = useState(null);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetchLanlord = async () => {
+      try {
+        const res = await fetch(`/api/user/${listing.userRef}`);
+        const data = await res.json();
+        setLandlord(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchLanlord();
+  }, [listing.userRef]);
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+  return (
+    <>
+      {landlord && (
+        <div className="flex flex-col gap-2">
+          <p>
+            Contact <span className="font-semibold">{landlord.username}</span>{" "}
+            for{" "}
+            <span className="font-semibold">{listing.name.toLowerCase()}</span>
+          </p>
+          <textarea
+            className="w-full border p-3 rounded-lg"
+            placeholder="Enter your message here..."
+            name="message"
+            id="message"
+            rows="2"
+            value={message}
+            onChange={handleChange}
+          ></textarea>
+          <Link
+            to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+            className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
+          >
+            Send Message
+          </Link>
+        </div>
+      )}
+    </>
+  );
+}
