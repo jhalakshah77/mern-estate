e116c89c0870ed5482d31c60e6403996fd0ea9e2
jhalakshah77 <jhalakshah77@gmail.com>
2024-04-23 20:51:46 +0400

    key prop inside map function

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -75,12 +75,12 @@ export default function Home() {
           offerListings.map((listing) => (
             <SwiperSlide>
               <div
+                key={listing._id}
                 style={{
                   background: `url(${listing.imageUrls[0]}) center no-repeat`,
                   backgroundSize: "cover",
                 }}
                 className="h-[500px]"
-                key={listing._id}
               ></div>
             </SwiperSlide>
           ))}
